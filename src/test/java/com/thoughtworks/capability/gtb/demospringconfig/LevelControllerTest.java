c93998dee18334fee44e8e17bdef64429c04c171
JINZHANG2017 <zj574406254@hotmail.com>
2020-10-27 22:26:27 +0800

    third test

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -0,0 +1,9 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LevelControllerTest {
+    //OS Environment Variable
+    //set levelNumber=1
+    //java -jar build/libs/demo-spring-config-0.0.1-SNAPSHOT.jar
+}
