9d2e47231a41513429df074d35e555637c082578
JINZHANG2017 <zj574406254@hotmail.com>
2020-10-27 22:13:37 +0800

    [ZhangJin] feat: second test

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -0,0 +1,8 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LevelControllerTest {
+    //方法二：Java System Property
+    // java -DlevelNumber=1 -jar build/libs/demo-spring-config-0.0.1-SNAPSHOT.jar
+}
